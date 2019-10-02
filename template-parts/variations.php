7287d3a1bace8246473e872c87f907d67c859465
mini <mini@minis-Mac-mini.home>
2019-10-02 22:45:28 +0100

    all variables style

--- template-parts/variations.php
+++ template-parts/variations.php
@@ -7,12 +7,16 @@ $available_variations = $product->get_available_variations();
 <div class="abcoffee_variations_wrap">
 <?php foreach($available_variations as $variation){?>
 	<?php
-		$variation_add_to_cart_href = get_site_url() . "/cart/?add-to-cart=" . $id . "&variation_id=" . $variation["variation_id"] . "&attribute_date=" . $variation["attributes"]["attribute_date"];
+		$the_date = $variation["attributes"]["attribute_date"];
+		$written_date_part1 = date('d');
+		$written_date_part2 = date('F Y');
+		$variation_add_to_cart_href = get_site_url() . "/cart/?add-to-cart=" . $id . "&variation_id=" . $variation["variation_id"] . "&attribute_date=" . $the_date;
 	?>
 	<div class="abcoffee_the_variation">
 		<div class="first_section">
 			<div class="date">
-				<?=$variation["attributes"]["attribute_date"]?>
+				<span><?=$written_date_part1?></span>
+				<span><?=$written_date_part2?></span>
 			</div>
 			<div class="name_and_level">
 				<p class="the_name"><?=$product->get_title();?></p>
@@ -20,23 +24,26 @@ $available_variations = $product->get_available_variations();
 					<?php if(isset($variation["image"]["url"])){?>
 						<img src="<?=$variation["image"]["url"]?>" alt="">
 					<?php }?>
-					<span><?=$variation["attributes"]["attribute_pa_degree-of-difficulty"]?></span>
+					<?php if(isset($variation["attributes"]["attribute_pa_degree-of-difficulty"])){?>
+						<span><?=$variation["attributes"]["attribute_pa_degree-of-difficulty"]?></span>
+					<?php }?>
 				</p>
 			</div>
 		</div>
 		<div class="seccond_section">
 			<div class="the_price">
-				<?=$product->get_price_html(); ?>
+				<?=$variation["display_price"]?>€
 			</div>
-			<div clas="availability">
+			<div class="availability">
 				<?=$variation["availability_html"]?>
 			</div>
 			<div class="the_add_to_cart_button_wrap">
-				<a class="event_add_to_cart" href="<?=$variation_add_to_cart_href?>">Inscrição</a>
+				<a class="variations_add_to_cart" href="<?=$variation_add_to_cart_href?>">Inscrição</a>
 			</div>
 		</div>
 			
 	</div>
+	<?php// var_dump($variation["display_price"]);?>
 <?php }?>
 
 </div>
